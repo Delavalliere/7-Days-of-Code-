f4b4be8d9061c5b54073262ead1b5c127604adc8
Delavalliere <93093162+Delavalliere@users.noreply.github.com>
2023-11-06 08:02:22 -0300

    Dia 2
    Nesse desafio consegui entender bastante a importância da crase, deixei de usa-la e tive problemas para que minha mensagem fosse exibida, preciso estudar um pouco mais sobre os sinais utilizados na programação. No começo, criei uma pagina toda estilizada, mas depois vi que não conseguiria fazer o link entre o 'form' no html com o Java Script, preciso estudar melhor essas operações também.

---
+++ Dia 2/app.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual seu nome?");
+const idade = prompt("Qual sua idade?");
+const linguagem = prompt("Qual linguagem você está aprendendo?");
+const mensagem = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`;
+
+alert (mensagem);
