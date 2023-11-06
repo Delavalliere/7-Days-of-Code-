48b37fe3ddddc7d8ac407111706824ace1004f81
Delavalliere <93093162+Delavalliere@users.noreply.github.com>
2023-11-06 08:16:51 -0300

    Add files via upload

--- Dia 2/app.js
+++ Dia 2/app.js
@@ -4,3 +4,17 @@ const linguagem = prompt("Qual linguagem você está aprendendo?");
 const mensagem = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`;
 
 alert (mensagem);
+
+const  pergunta = prompt (`'Você gosta de estudar ${linguagem}? Responda com 1 se Sim ou 2 se Não!'`);
+
+    if (pergunta == 1){
+
+        alert ("Que legal! Continue estudando e você terá muito sucesso!")
+
+    }
+
+    if (pergunta == 2){
+
+        alert ("Que pena! Espero que continue se aventurando na programação, quem sabe você não encontra outra linguagem que te agrade? :)")
+    }
+
